c8cdb2e58890a3aaa646c41eb78d8684d2a25efc
Francis Albuna <falbuna1@gmail.com>
2020-10-02 19:16:49 -0700

    Added styling to index handlebars.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (id, burger_name) VALUES (1, 'Cheese Burger');
-INSERT INTO burgers (id, burger_name, devoured) VALUES (2, 'Bacon Cheese Burger', 1);
-INSERT INTO burgers (id, burger_name) VALUES (3, 'Double Cheese Burger');
+INSERT INTO burgers (id, burger_name) VALUES (1, 'Cheeseburger');
+INSERT INTO burgers (id, burger_name, devoured) VALUES (2, 'Bacon Cheeseburger', 1);
+INSERT INTO burgers (id, burger_name) VALUES (3, 'Double Cheeseburger');
